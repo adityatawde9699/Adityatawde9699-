d07605f92782bcff509a0f81c55037f4e3658ef9
aditya tawde <adityatawde9699@gmail.com>
2024-10-29 22:44:14 +0530

    Update script.js
    removed the dark mode function

--- script.js
+++ script.js
@@ -8,15 +8,3 @@ hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.setAttribute('aria-expanded', !expanded);
     mobileNav.setAttribute('aria-expanded', !expanded);
 });
-
-function toggleMode() {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
-}
-
-function loadTheme() {
-    const theme = localStorage.getItem('theme');
-    if (theme === 'dark') {
-        document.body.classList.add('dark-mode');
-    }
-}
